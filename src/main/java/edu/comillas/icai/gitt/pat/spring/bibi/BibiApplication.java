6b952d80f17f1eade4ea3147ef3b10fd7e7438cb
Bibi <nuchi@MacBook-Pro-de-Bibi.local>
2026-02-07 11:52:20 +0100

    init project

---
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/bibi/BibiApplication.java
@@ -0,0 +1,13 @@
+package edu.comillas.icai.gitt.pat.spring.bibi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/comillas/icai/gitt/pat/spring/bibi/BibiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.comillas.icai.gitt.pat.spring.bibi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
